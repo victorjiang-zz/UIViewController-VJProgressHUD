9fd9571f5f0cc1d0a10badce0a2c69779226dda5
victor.jiang <victor.jiang@softtek.com>
2015-06-03 09:41:17 +0800

    Initial Commit

---
+++ UIViewController+VJProgressHUD/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UIViewController+VJProgressHUD
+//
+//  Created by Victor Jiang on 6/3/15.
+//  Copyright (c) 2015 Victor Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UIViewController+VJProgressHUDTests/UIViewController_VJProgressHUDTests.m
@@ -0,0 +1,40 @@
+//
+//  UIViewController_VJProgressHUDTests.m
+//  UIViewController+VJProgressHUDTests
+//
+//  Created by Victor Jiang on 6/3/15.
+//  Copyright (c) 2015 Victor Jiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface UIViewController_VJProgressHUDTests : XCTestCase
+
+@end
+
+@implementation UIViewController_VJProgressHUDTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
